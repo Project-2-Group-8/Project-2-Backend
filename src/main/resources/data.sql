e7e01c09320b8bdd4c97e70174a8eb1c304ba38e
Brandon Algarra <brandon.algarra@gmail.com>
2026-03-05 15:10:27 -0800

    Added a few test inserts to data.sql, renamed hikecontroller for better clarity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO hike_logs (trail_name, distance_miles, user_email, activity_type, duration_minutes, created_at)
 VALUES ('Morning Trail', 5.2, 'user1@example.com', 'Walking', 60, CURRENT_TIMESTAMP);
+
+INSERT INTO hike_logs ( trail_name, distance_miles, user_email, activity_type, duration_minutes, created_at)
+VALUES ('Morning Trail2', 5.2, 'user1@example.com', 'Walking', 60, CURRENT_TIMESTAMP);
+
+INSERT INTO hike_logs ( trail_name, distance_miles, user_email, activity_type, duration_minutes, created_at)
+VALUES ( 'Morning Trail3', 5.2, 'user1@example.com', 'Walking', 60, CURRENT_TIMESTAMP);
